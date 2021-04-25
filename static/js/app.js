0b837155b136020ad55a5b4951583f23313611b2
Pam M <22107270+pammeyer80@users.noreply.github.com>
2021-04-25 15:42:04 -0500

    initial check-in, including dropdown values

---
+++ static/js/app.js
@@ -0,0 +1,23 @@
+//Portions of code are from the instructor's office hours tutorial.
+console.log("app.js loaded");
+
+function init() {
+    //Populate dropdown
+    var dropdown = d3.select("#selDataset");
+
+    d3.json("data/samples.json").then(d => {
+        console.log(d);
+
+        var sampleNames = d.names;
+
+        sampleNames.forEach(sample => {
+            dropdown.append("option")
+                .text(sample)
+                .property("value", sample);
+        });
+
+
+    });
+};
+
+init();
