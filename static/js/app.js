f9d763fc0e2fd5b66102226c6d093a4d4d0608d6
Pam M <22107270+pammeyer80@users.noreply.github.com>
2021-04-25 17:10:41 -0500

    add bar graph

--- static/js/app.js
+++ static/js/app.js
@@ -34,13 +34,69 @@ function optionSelected(selectedSample){
 };
 
 function drawGraph(selectedSample){
-    console.log("Draw Bargraph");
+    console.log(`Draw Bargraph for ${selectedSample}`);
+
+    d3.json("data/samples.json").then(d => {
+
+        var samples = d.samples;
+        var resultArray = samples.filter(s => s.id == selectedSample);
+        var result = resultArray[0];
+
+        var otu_ids = result.otu_ids;
+        var otu_labels = result.otu_labels;
+        var sample_values = result.sample_values;
+
+        
+        xticks = sample_values.slice(0,10).reverse();
+        yticks = otu_ids.slice(0,10).map(otuID => `OTU ${otuID}`).reverse();
+
+        var barTrace = {
+            x: xticks,
+            y: yticks,
+            type: "bar",
+            text: otu_labels.slice(0,10).reverse(),
+            orientation: "h"
+        }
+
+        var barData = [barTrace];
+
+        var barLayout = {
+            title: "Top 10 Bacteria Cultures Found",
+            margin: {t:30, l:150}
+        }
+
+        Plotly.newPlot("bar", barData, barLayout);
+        console.log(result);
+
+    });
+
 };
 function drawChart(selectedSample){
-    console.log("Draw Bubble Chart");
+    console.log(`Draw Bubble Chart for ${selectedSample}`);
+
+    // d3.json("data/samples.json").then(d => {
+
+    //     var samples = d.samples;
+    //     var resultArray = samples.filter(s => s.id == selectedSample);
+    //     var result = resultArray[0];
+
+    //     console.log(result);
+
+    // });
+
 };
 function getDemoData(selectedSample){
-    console.log("Show Demographic Data");
+    console.log(`Show Demographic Data for ${selectedSample}`);
+
+    // d3.json("data/samples.json").then(d => {
+
+    //     var metadata = d.metadata;
+    //     var resultArray = metadata.filter(m => m.id == selectedSample);
+    //     var result = resultArray[0];
+
+    //     console.log(result);
+
+    // });
 
 };
 
